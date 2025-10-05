3056df8970ad9d0093eea93c86a4f41adcce24b1
Егор Королев <Tyuiu.KorolevES@outlook.com>
2025-10-05 09:55:38 +0500

    4

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Discription { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+    }
+}
